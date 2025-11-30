c3e21f28eba9e82181e8325403c2e77c3c17d36c
Rodrigo-Iasculski <rodrigoiasculski@gmail.com>
2025-11-30 18:21:07 -0300

    Fix issues, added case study

--- main.hs
+++ main.hs
@@ -1,4 +1,3 @@
---- Definição das árvore sintática para representação dos programas:
 data E = Num Int
       |Var String
       |Soma E E
@@ -29,31 +28,23 @@ data C = While B C
     | Swap E E
    deriving(Eq,Show)                
 --- A próxima linha de código diz que o tipo memória é equivalente a uma lista de tuplas, onde o
---- primeiro elemento da tupla é uma String (nome da variável) e o segundo um Inteiro
---- (conteúdo da variável):
+--- primeiro elemento da tupla é uma String (nome da variável) e o segundo um Inteiro (conteúdo da variável):
 type Memoria = [(String,Int)]
 
 exSigma :: Memoria
 exSigma = [ ("x", 10), ("temp",0), ("y",0)]
 
---- A função procuraVar recebe uma memória, o nome de uma variável e retorna o conteúdo
---- dessa variável na memória. Exemplo:
+--- A função procuraVar recebe uma memória, o nome de uma variável e retorna o conteúdo dessa variável na memória. Exemplo:
 procuraVar :: Memoria -> String -> Int
 procuraVar [] s = error ("Variavel " ++ s ++ " nao definida no estado")
 procuraVar ((s,i):xs) v
   | s == v     = i
   | otherwise  = procuraVar xs v
 
-
 --- A função mudaVar, recebe uma memória, o nome de uma variável e um novo conteúdo para essa
---- variável e devolve uma nova memória modificada com a varíável contendo o novo conteúdo. A
---- chamada
----
+--- variável e devolve uma nova memória modificada com a varíável contendo o novo conteúdo. Exemplo:
 --- *Main> mudaVar exSigma "temp" 20
 --- [("x",10),("temp",20),("y",0)]
----
----
---- essa chamada é equivalente a operação exSigma[temp->20]
 
 mudaVar :: Memoria -> String -> Int -> Memoria
 mudaVar [] v n = error ("Variavel " ++ v ++ " nao definida no estado")
@@ -115,53 +106,52 @@ cbigStep (TenTimes c,s) = cbigStep(Loop (Num 0) (Num 10) c,s)
 
 cbigStep (Repeat c b,s)
  | bbigStep(b,s0) = (Skip,s0)
- | otherwise = (While (Not b) c,s0)
+ | otherwise = cbigStep(Repeat c b,s0) 
  where
    (_,s0) = cbigStep(c,s)
 
 cbigStep(Loop e1 e2 c,s)
  | ebigStep(e2,s) - ebigStep(e1,s) > 0 = cbigStep(Seq c (Loop e1 (Sub e2 (Num 1)) c),s)
  | otherwise = (Skip,s)
+
 cbigStep(DuplaATrib x y e1 e2,s) = cbigStep(Seq (Atrib x e1) (Atrib y e2),s) 
 
-cbigStep (AtribCond b x e1 e2,s)
- | bbigStep(b,s) = cbigStep((Atrib x e1,s))
+cbigStep (AtribCond b x e1 e2,s) 
+ | bbigStep(b,s) = cbigStep((Atrib x e1,s))  
  | otherwise = cbigStep((Atrib x e2,s))
+   
+cbigStep(Swap x y,s) = cbigStep(DuplaATrib x y (Num s1) (Num c1),s)
+ where (c1,s1) = ((ebigStep(Var "x",s)),(ebigStep(Var "y",s)))
+----------------------------------------------------------------------------------------------------  
+---Testes
+---------------------------------------------------------------------------------------------------- 
 
---TERMINAR
-cbigStep (Swap x y,s)
- | x == y = cbigStep(Seq(Atrib x y) (Atrib y x),s)
- | otherwise = cbigStep(Seq(Atrib x y) (Atrib y x),s0)
- where
-   (_,s0) = cbigStep(Atrib y (Num 2),s)
------------------------------------------------------------------------------------------------------
-exSigma2 :: Memoria
-exSigma2 = [("x",3), ("y",0), ("z",0)]
+-- Loop
+sigmaQuadrado :: Memoria
+sigmaQuadrado = [("x",1)]
 
-progExp1 :: E
-progExp1 = Soma (Num 3) (Soma (Var "x") (Var "y"))
+exp10 :: C
+exp10 = (Loop (Num 0)(Num 10) (Atrib (Var "x") (Mult (Var "x")(Num 3))))
 
-teste1 :: B
-teste1 = (Leq (Soma (Num 3) (Num 3))  (Mult (Num 2) (Num 3)))
+-- Repeat
+sigmaRepeat :: Memoria
+sigmaRepeat = [("x",1),("y",2)]
 
-teste2 :: B
-teste2 = (Leq (Soma (Var "x") (Num 3))  (Mult (Num 2) (Num 3)))
+repeatUntil :: C
+repeatUntil = (Repeat (Seq (Atrib (Var "x") (Soma (Var "x") (Var "y")))
+                           (Atrib (Var "y") (Soma (Var "y") (Num 1))))
+                       (Igual (Var "y") (Num 101)))
 
-testec1 :: C
-testec1 = (Seq (Seq (Atrib (Var "z") (Var "x")) (Atrib (Var "x") (Var "y"))) (Atrib (Var "y") (Var "z")))
+-- Dupla Atrib, Swap e AtribCond
 
-fatorial :: C  
-fatorial = (Seq (Atrib (Var "y") (Num 1))
-                (While (Not (Igual (Var "x") (Num 1)))
-                       (Seq (Atrib (Var "y") (Mult (Var "y") (Var "x")))
-                            (Atrib (Var "x") (Sub (Var "x") (Num 1))))))
+sigmaAtrib :: Memoria
+sigmaAtrib = [("x",0)]
 
+atrib2 :: C
+atrib2 = (DuplaATrib (Var "x") (Var "y") (Num 5) (Num 3))
 
+sw :: C
+sw = (Swap (Var "x") (Var "y")) 
 
-
-
-
-
---cbigStep (DuplaATrib (Var "x") (Var "y") (Num 5) (Num 3) , ([("x",0 ),("y",0)]))
---cbigStep (AtribCond FALSE (Var "x") (Num 3) (Num 5) , [("x",0)])
---cbigStep (Loop (Num 1)(Num 3) (Atrib (Var "x") (Soma (Var "x")(Num 1) )) , [("x",0)])
+atribCond :: C
+atribCond = (AtribCond FALSE (Var "x") (Num 10) (Num 20))
